6fcc5b332706c453d674d74e6dd4ba0f84ab253b
a14samvilcad <a14samvilcad@inspedralbes.cat>
2023-02-01 12:30:11 +0100

    button play

--- Android/core/src/m08/uf3/drops/Screens/MainMenuScreen.java
+++ Android/core/src/m08/uf3/drops/Screens/MainMenuScreen.java
@@ -1,31 +1,39 @@
 package m08.uf3.drops.Screens;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
 
 import m08.uf3.drops.Drops;
-import m08.uf3.drops.Screens.GameScreen;
 import m08.uf3.drops.Utils.Settings;
 
 public class MainMenuScreen implements Screen {
 
     final Drops game;
+    GameScreen gameScreen;
     OrthographicCamera camera;
     Stage stage;
     int lives;
     Label title;
     Label message;
 
+    TextButton button;
+
+
     public MainMenuScreen(Drops game) {
         this.game = game;
+
         lives = 3;
 
         // Creem la càmera de les dimensions del joc
@@ -37,23 +45,35 @@ public class MainMenuScreen implements Screen {
 
         // Creem l'stage i assginem el viewport
         stage = new Stage(viewport);
+        gameScreen = new GameScreen(stage.getBatch(), stage.getViewport(), game);
+
         crearLabels();
-        stage.addActor(title);
-        stage.addActor(message);
+        //stage.addActor(title);
+        //stage.addActor(message);
+        stage.addActor(button);
     }
 
     private void crearLabels(){
-        BitmapFont bitmapfont = new BitmapFont();
-        title = new Label("Zombie Game", new Label.LabelStyle(bitmapfont, Color.WHITE));
+        Gdx.input.setInputProcessor(stage);
+        BitmapFont font = new BitmapFont();
+        title = new Label("PLAY", new Label.LabelStyle(font, Color.WHITE));
         title.setFontScale(Settings.TITLE_RESCALE_SIZE);
-        title.setPosition((Settings.GAME_WIDTH - (title.getWidth() * Settings.TITLE_RESCALE_SIZE)) / 2, ((Settings.GAME_HEIGHT - title.getHeight()) / 2) + 50);
-        message = new Label("Pulsa en la pantalla para empezar", new Label.LabelStyle(bitmapfont, Color.WHITE));
-        message.setPosition((Settings.GAME_WIDTH - (message.getWidth() * Settings.TITLE_RESCALE_SIZE)) / 2, (Settings.GAME_HEIGHT - message.getHeight()) / 2);
+        /*title.setPosition((Settings.GAME_WIDTH - (title.getWidth() * Settings.TITLE_RESCALE_SIZE)) / 2, ((Settings.GAME_HEIGHT - title.getHeight()) / 2) + 50);
+        message = new Label("Pulsa en la pantalla para empezar", new Label.LabelStyle(font, Color.WHITE));
+        message.setPosition((Settings.GAME_WIDTH - (message.getWidth() * Settings.TITLE_RESCALE_SIZE)) / 2, (Settings.GAME_HEIGHT - message.getHeight()) / 2);*/
+
+        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
+        textButtonStyle.font = font;
+        textButtonStyle.fontColor = Color.GREEN;
+        button = new TextButton("PLAY", textButtonStyle);
+        button.setLabel(title);
+        button.setPosition((Settings.GAME_WIDTH - (button.getWidth() * Settings.TITLE_RESCALE_SIZE)) / 2, (Settings.GAME_HEIGHT - button.getHeight()) / 1.1f);
+
+
     }
 
     @Override
     public void show() {
-
     }
 
     @Override
@@ -62,11 +82,18 @@ public class MainMenuScreen implements Screen {
 
         stage.draw();
         stage.act(delta);
+        if (button.addCaptureListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                game.setScreen(gameScreen);
+            }
+        }));
+
         // Si es fa clic en la pantalla, canviem la pantalla
-        if (Gdx.input.isTouched()) {
+        /*if (Gdx.input.isTouched()) {
             game.setScreen(new GameScreen(stage.getBatch(), stage.getViewport(), game));
             dispose();
-        }
+        }*/
     }
 
     @Override
@@ -91,7 +118,7 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void dispose() {
-
+        game.dispose();
     }
 
 
--- Android/core/src/m08/uf3/drops/Utils/Settings.java
+++ Android/core/src/m08/uf3/drops/Utils/Settings.java
@@ -6,7 +6,7 @@ public class Settings {
     public static final int GAME_WIDTH = 800;
     public static final int GAME_HEIGHT = 480;
     public static float WALLET_VELOCITY = 200;
-    public static float TITLE_RESCALE_SIZE = 3;
+    public static float TITLE_RESCALE_SIZE = 2;
     public static int LIVES = 3;
     public static final float PLAYER_VELOCITY = 250;
 
